85432e7abd3425ff9aea5e0b1203acb41a15f15e
12203939 <153032196+12203939@users.noreply.github.com>
2023-12-16 04:21:34 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,13 @@
-# Streamlitライブラリをインポート
 import streamlit as st
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+# タイトルの表示
+st.title('確率の計算')
 
-# タイトルを設定
-st.title('Streamlitのサンプルアプリ')
+# 数値の入力
+input_number = st.number_input('数値を入力してください', min_value=0)
 
-# テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
-user_input = st.text_input('あなたの名前を入力してください')
+# 確率の計算
+probability = input_number / 100  # 仮の確率計算例
 
-# ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
-    if user_input:  # 名前が入力されているかチェック
-        st.success(f'🌟 こんにちは、{user_input}さん! 🌟')  # メッセージをハイライト
-    else:
-        st.error('名前を入力してください。')  # エラーメッセージを表示
-
-# スライダーを作成し、値を選択
-number = st.slider('好きな数字（10進数）を選んでください', 0, 100)
-
-# 補足メッセージ
-st.caption("十字キー（左右）でも調整できます。")
-
-# 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
-
-# 選択した数値を2進数に変換
-binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
-st.info(f'🔢 10進数の「{number}」を2進数で表現すると「{binary_representation}」になります。 🔢')  # 2進数の表示をハイライト
+# 結果の表示
+st.write(f"入力した数値に対応する確率は {probability} です。")
